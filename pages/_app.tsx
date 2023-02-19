27d1aae6e01f67d3b8b3235dd32ffde610fba41c
smalandrey67 <smalandrey67@ukr.net>
2023-02-19 22:26:21 +0200

    #3: install chefmaster-ui, connect fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
+import { Poppins } from "@next/font/google";
+
 import type { AppProps } from "next/app";
 
+import "chefmaster-ui/dist/styles/main.css";
 import "../styles/globals.scss";
 
+const poppins = Poppins({
+	subsets: ["latin"],
+	weight: ["400", "600", "800"]
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<style jsx global>
+				{`
+					:root {
+						--poppins-font: ${poppins.style.fontFamily};
+					}
+				`}
+			</style>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
