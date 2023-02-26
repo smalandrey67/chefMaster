9cd492f40c0de82cc923a4dc6c9a6ad07c4a2f48
smalandrey67 <smalandrey67@ukr.net>
2023-02-26 17:59:53 +0200

    #fix: change name of importing interface

--- app/modules/Navbar/components/NavbarItem/NavbarItem.interface.ts
+++ app/modules/Navbar/components/NavbarItem/NavbarItem.interface.ts
@@ -1,3 +1,3 @@
-import type { NavbarItems } from "../../Navbar.interface";
+import type { NavbarItem } from "../../Navbar.interface";
 
-export interface NavbarItemProps extends Omit<NavbarItems, "id"> {}
+export interface NavbarItemProps extends Omit<NavbarItem, "id"> {}
