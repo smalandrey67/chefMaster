fc71c91f55ed1221c761ca027512a3f2d5146ed4
smalandrey67 <smalandrey67@ukr.net>
2023-02-27 15:14:44 +0200

    add two charts in PersonalStatistic module

--- app/modules/PersonalStatistic/PersonalStatistic.tsx
+++ app/modules/PersonalStatistic/PersonalStatistic.tsx
@@ -1,5 +1,59 @@
+import { Chart as ChartJS, RadialLinearScale, ArcElement, Tooltip, Legend, ChartData } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
+
 import styles from "./PersonalStatistic.module.scss";
 
 export function PersonalStatistic(): JSX.Element {
-	return <div className={styles.personalStatistic}>PersonalStatistic</div>;
+	const calories = {
+		max: 800,
+		current: 300
+	};
+
+	const carbohydrates = {
+		max: 2000,
+		current: 1200
+	};
+
+	const caloriesData: ChartData<"doughnut"> = {
+		labels: ["calories left", "calories"],
+		datasets: [
+			{
+				data: [calories.max, calories.current],
+				backgroundColor: ["#212121", "#edb200"],
+				borderColor: ["#212121", "#edb200"]
+			}
+		]
+	};
+
+	const carbohydratesData: ChartData<"doughnut"> = {
+		labels: ["carbohydrates left", "carbohydrates"],
+		datasets: [
+			{
+				data: [carbohydrates.max, carbohydrates.current],
+				backgroundColor: ["#212121", "#edb200"],
+				borderColor: ["#212121", "#edb200"]
+			}
+		]
+	};
+
+	const options = {
+		plugins: {
+			legend: {
+				display: false
+			}
+		}
+	};
+
+	return (
+		<div className={styles.statistic}>
+			<div className={styles.statisticItem}>
+				<Doughnut data={caloriesData} options={options} />
+			</div>
+			<div className={styles.statisticItem}>
+				<Doughnut data={carbohydratesData} options={options} />
+			</div>
+		</div>
+	);
 }
